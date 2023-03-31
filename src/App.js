49155e56011fc38ad536d50bb3e16eaed6eb695b
Thuan <nguyenmthuan18@gmail.com>
2023-03-31 20:50:38 +0700

    Content

--- src/App.js
+++ src/App.js
@@ -7,9 +7,26 @@ function App() {
   return (
     <Router>
       <Header></Header>
-      <div>
-        <Sidebar></Sidebar>
-        {/* Content */}
+      <div style={{ display: 'flex' }}>
+        <Sidebar />
+        <div>
+          <Routes>
+            <Route path='/' element={<h1>Home</h1>} />
+            <Route path="/content1" element={<h1>Content1</h1>} />
+            <Route path="/content2" element={<h1>Content2</h1>} />
+            <Route path="/content3" element={<h1>Content3</h1>} />
+            <Route path="/content4" element={<h1>Content4</h1>} />
+            <Route path="/content5" element={<h1>Content5</h1>} />
+            <Route path="/content6" element={<h1>Content6</h1>} />
+            <Route path="/content7" element={<h1>Content7</h1>} />
+            <Route path="/content8" element={<h1>Content8</h1>} />
+            <Route path="/content9" element={<h1>Content9</h1>} />
+            <Route path="/content10" element={<h1>Content10</h1>} />
+            <Route path="/content11" element={<h1>Content11</h1>} />
+            <Route path="/content12" element={<h1>Content12</h1>} />
+            <Route path="/content13" element={<h1>Content13</h1>} />
+          </Routes>
+        </div>
       </div>
     </Router>
   );
